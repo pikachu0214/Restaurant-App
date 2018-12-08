08a862d688336dafaf1f59906af04cf61ddb895f
Danny Yen <danny.y.yen@gmail.com>
2018-12-08 16:55:52 +0200

    Added user signup and authentication

--- components/Screens/Appetizer.js
+++ components/Screens/Appetizer.js
@@ -1,16 +1,47 @@
 import React, { Component } from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, FlatList } from "react-native";
 import { ScrollView } from "react-native-gesture-handler";
 import Header from "./Contents/Header";
-
+import * as firebase from "firebase";
 export default class Appetizer extends Component {
+  state = { data: [] };
   static navigationOptions = { title: "Appetizer" };
+
+  componentWillMount() {
+    
+  }
+
+  listSeparator = () => {
+    return (
+      <View
+        style={{
+          height: 5,
+          width: "80%",
+          backgroundColor: "#fff",
+          marginLeft: "10%"
+        }}
+      />
+    );
+  };
+
   render() {
     return (
       <ScrollView style={styles.container}>
         <Header />
         <View>
-        <Text>Appetizer Screen</Text>
+          <FlatList
+            style={{ marginLeft: "5%" }}
+            keyExtractor={item => item.id}
+            renderItem={({ item }) => (
+              <View style={styles.listcontainer}>
+                <Text style={{ fontSize: 18, color: "blue" }}>
+                  {item.field}, {item.value}{" "}
+                </Text>
+              </View>
+            )}
+            data={this.state.data}
+            ItemSeparatorComponent={this.listSeparator}
+          />
         </View>
       </ScrollView>
     );
--- components/Screens/Login.js
+++ components/Screens/Login.js
@@ -1,23 +1,99 @@
 import React from "react";
-import { StyleSheet, Text, View, Button } from "react-native";
+import { StyleSheet, Text, View, Alert } from "react-native";
+import { Container, Content, Header, Form, Input, Item, Button} from "native-base"
 import Home from "./Home";
 import LoginBanner from "./LoginBanner";
 import { ScrollView } from "react-native-gesture-handler";
+import * as firebase from 'firebase'
+
+const config = {
+    apiKey: "AIzaSyBwWm-UvF-NjqrGc4hahB-cXp_CdB-tC2I",
+    authDomain: "restaurant-app-42260.firebaseapp.com",
+    databaseURL: "https://restaurant-app-42260.firebaseio.com",
+    projectId: "restaurant-app-42260",
+    storageBucket: "restaurant-app-42260.appspot.com",
+    messagingSenderId: "513052896710"
+  };
+
+  firebase.initializeApp(config);
 
 export default class Login extends React.Component {  
+  state = {email: "", password: ""};
+
+  
+  //Method to sign user with email and password
+  signUpUser = (email, password) => {
+    try{
+      if(this.state.password.length<6){
+        alert("Please enter at least 6 character")
+        return;
+      }
+        firebase.auth().createUserWithEmailAndPassword(email, password).then(function(user){
+        Alert.alert("Successfully signed up!")
+        })
+    }
+    catch(error){
+      console.log(error.toString())
+    }
+  };
+
+  //Method to log user with email and password
+  loginUser = (email, password) => {
+    try{
+      firebase.auth().signInWithEmailAndPassword(email, password)
+      .then(() => this.props.navigation.navigate('Home'))
+    }
+    catch(error){
+      console.log(error.toString())
+    }
+  };
   render() {
     const { navigate } = this.props.navigation;
     return (
-      <ScrollView>
-        <LoginBanner />
-        <Button onPress={() => navigate("Home")} title="Signin" />
-      </ScrollView>
+        <Container style={styles.container}>
+        <Form>
+          <Item>
+            <Input
+            autoCorrect={false}
+            autoCapitalize="none"
+            placeholder="Email"
+            onChangeText={(email) => this.setState({email})}
+            />
+          </Item>
+          <Item>
+            <Input
+            autoCorrect={false}
+            autoCapitalize="none"
+            secureTextEntry={true}
+            placeholder="Password"
+            onChangeText={(password) => this.setState({password})}
+
+            />
+          </Item>
+          {/*Login Button*/}
+          <Button style={{marginTop: 10}} full rounded success
+          onPress={() => this.loginUser(this.state.email, this.state.password)}
+          >
+          <Text style={{color: "white"}}>Login</Text>
+          </Button>
+          {/*Sign Up Button*/}
+          <Button style={{marginTop: 10}} full rounded primary
+          onPress={() => this.signUpUser(this.state.email, this.state.password)}
+          >
+          <Text style={{color: "white"}}>Sign Up</Text>
+          </Button>
+        </Form>
+        </Container>
     );
   }
 }
 const styles = StyleSheet.create({
   container: {
+    flex: 1,
+    backgroundColor: "#fff",
     justifyContent: "center",
-    alignItems: "center"
-  }
+    padding: 10,
+    marginBottom: 300,
+  },
+  
 });
